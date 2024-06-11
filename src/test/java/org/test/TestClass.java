5534a6325ebd297830c40d3e93bea1bd4ea7518d
cgaetan <gaetan.chaboussie@nereide.fr>
2024-06-11 09:41:34 +0200

    Adds example test class

---
+++ src/test/java/org/test/TestClass.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.junit.Test;
+
+public class TestClass {
+
+    @Test
+    public void test() {
+        String foo = "bar";
+        assert foo.equals("bar");
+    }
+}
